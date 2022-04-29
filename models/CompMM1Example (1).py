3b204192982f5f01fe7ed3fd500a9abe89d7212b
andi-ra <adulla95@gmail.com>
2022-04-29 10:55:00 +0200

    Aggiustato le simulazioni a qualcosa di utile da far vedere

--- models/CompMM1Example (1).py
+++ models/CompMM1Example (1).py
@@ -13,50 +13,48 @@ if __name__ == '__main__':
     # Set up arrival and packet size distributions
     # Using Python functools to create callable functions for random variates with fixed parameters.
     # each call to these will produce a new random value.
-    adist = functools.partial(random.expovariate, 0.005)
-    sdist = functools.partial(random.expovariate, 0.0001)  # mean size 100 bytes
-    samp_dist = functools.partial(random.expovariate, 2.5)
-    port_rate = 1000.0
+    adist = functools.partial(random.expovariate, 0.01)
+    sdist = functools.partial(random.expovariate, 0.05)  # mean size 100 bytes
+    samp_dist = functools.partial(random.expovariate, 0.005)
+    port_rate = 1.5
 
     env = simpy.Environment()  # Create the SimPy environment
     # Create the packet generators and sink
     ps = PacketSink(env, debug=False, rec_arrivals=True)
     pg = PacketGenerator(env, "Greg", adist, sdist)
-    switch_port = SwitchPort(env, port_rate, qlimit=10000)
+    switch_port = SwitchPort(env, port_rate, qlimit=100000)
     # Using a PortMonitor to track queue sizes over time
     pm = PortMonitor(env, switch_port, samp_dist)
     # Wire packet generators, switch ports, and sinks together
     pg.out = switch_port
     switch_port.out = ps
     # Run it
-    env.run(until=8000)
+    env.run(until=8000000)
     print("Last 10 waits: " + ", ".join(["{:.3f}".format(x) for x in ps.waits[-10:]]))
-    print("Last 10 queue sizes: {}".format(pm.sizes[0:10000]))
+    print("Last 10 queue sizes: {}".format(pm.sizes[-1000:]))
+
+    fig, axis = plt.subplots()
+    axis.plot(pm.sizes)
+    axis.set_title("Line plot for System Occupation")
+    axis.set_xlabel("time")
+    axis.set_ylabel("customers in system")
+    plt.show()
+
     print("Last 10 sink arrival times: " + ", ".join(["{:.3f}".format(x) for x in ps.arrivals[-10:]]))
     print("average wait = {:.3f}".format(sum(ps.waits) / len(ps.waits)))
     print(
         "received: {}, dropped {}, sent {}".format(switch_port.packets_rec, switch_port.packets_drop, pg.packets_sent))
     print("loss rate: {}".format(float(switch_port.packets_drop) / switch_port.packets_rec))
     print("average system occupancy: {:.3f}".format(float(sum(pm.sizes)) / len(pm.sizes)))
-
-    # fig, axis = plt.subplots()
-
-    # axis.set_title("Histogram for waiting times")
-    # axis.set_xlabel("time")
-    # axis.set_ylabel("normalized frequency of occurrence")
-    # fig.savefig("WaitHistogram.png")
-    # plt.show()
-    # fig, axis = plt.subplots()
-    # axis.hist(ps.waits, bins=100, normed=True)
-    # axis.set_title("Histogram for System Occupation times")
-    # axis.set_xlabel("number")
-    # axis.set_ylabel("normalized frequency of occurrence")
-    # fig.savefig("QueueHistogram.png")
-    # plt.show()
     fig, axis = plt.subplots()
-    axis.hist(ps.arrivals, bins=100, )
+    axis.hist(ps.waits, bins=10000, density=True)
+    axis.set_title("Histogram for Sojourn times")
+    axis.set_xlabel("number")
+    axis.set_ylabel("normalized frequency of occurrence")
+    plt.show()
+    fig, axis = plt.subplots()
+    axis.hist(ps.arrivals, bins=100, density=True)
     axis.set_title("Histogram for Sink Interarrival times")
     axis.set_xlabel("time")
     axis.set_ylabel("normalized frequency of occurrence")
-    # fig.savefig("ArrivalHistogram.png")
     plt.show()
---
+++ tesi-andi/.ipynb_checkpoints/intro-checkpoint.md
@@ -0,0 +1,22 @@
+# Strategie di network forming con tecniche SDN per reti di sensori per applicazioni di mobile IoT
+
+In questo documento mi accingo a mostrare le strategie di network forming. Analizzare la risposta alle domande: 
+* perché le reti di sensori hanno certe topologie particolari?
+* Cosa comporta la creazione di una rete di sensori?
+* È sufficiente soltanto creare la rete di sensori e questa funzionerà correttamente per tutto il suo ciclo di vita?
+* Cosa sappiamo del controllo delle reti? 
+* È necessario formare la rete in un certo modo? Possiamo controllare questo processo?
+
+Questo documento è stato sritto con Jupyter Books [the Jupyter Book documentation](https://jupyterbook.org) seguire il link per maggiori info.
+
+Ecco l'indice dei contnuti:
+
+```{tableofcontents}
+- Introduzione e motivazioni
+- Stato dell'arte SDN e datastore
+- Stato dell'arte algoritmi ottimizzazione
+- Controller Ryu e NetApp
+- Algoritmi di ottimizzazione
+- Simulazione e risultati
+- Conclusione
+```
